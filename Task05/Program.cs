2807f0372bb5bba3ba64e338553df8fc0c733b95
Fedor Romanov <flauto86@gmail.com>
2023-05-24 19:41:35 +0300

    Edit Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -7,9 +7,13 @@
 Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int i = -number;
-while (i <= number)
-{
-    Console.Write($"{i} ");
-    i++;
+if (number < 1)
+    Console.WriteLine("Некорректный ввод");
+else{
+    int i = -number;
+    while (i <= number)
+    {
+        Console.Write($"{i} ");
+        i++;
+    }
 }
