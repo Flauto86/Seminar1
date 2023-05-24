52332a6202f1a6d2d25a5e08f946e5bb1b71de05
Fedor Romanov <flauto86@gmail.com>
2023-05-24 19:03:16 +0300

    Variant 2

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -3,41 +3,79 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.Write("Введите число, соответствующее дню недели: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число, соответствующее дню недели: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-if (num >= 1 && num <= 7)
+// if (num >= 1 && num <= 7)
+// {
+//     if (num == 1)
+//     {
+//     Console.WriteLine("Понедельник");
+//     }
+//     else if (num == 2)
+//     {
+//     Console.WriteLine("Вторник");
+//     }
+//     else if (num == 3)
+//     {
+//     Console.WriteLine("Среда");
+//     }
+//     else if (num == 4)
+//     {
+//     Console.WriteLine("Четверг");
+//     }
+//     else if (num == 5)
+//     {
+//     Console.WriteLine("Пятница");
+//     }
+//     else if (num == 6)
+//     {
+//     Console.WriteLine("Суббота");
+//     }
+//     else if (num == 7)
+//     {
+//     Console.WriteLine("Воскресенье");
+//     }
+// }
+// else
+// {
+// Console.WriteLine("Введено неправильное число.");
+// }
+
+
+
+Console.Write("Введите число, соответствующее дню недели: ");
+string num = Console.ReadLine();
+
+if (num == "1")
 {
-    if (num == 1)
-    {
-    Console.WriteLine("Понедельник");
-    }
-    else if (num == 2)
-    {
-    Console.WriteLine("Вторник");
-    }
-    else if (num == 3)
-    {
-    Console.WriteLine("Среда");
-    }
-    else if (num == 4)
-    {
-    Console.WriteLine("Четверг");
-    }
-    else if (num == 5)
-    {
-    Console.WriteLine("Пятница");
-    }
-    else if (num == 6)
-    {
-    Console.WriteLine("Суббота");
-    }
-    else if (num == 7)
-    {
-    Console.WriteLine("Воскресенье");
-    }
+Console.WriteLine("Понедельник");
+}
+else if (num == "2")
+{
+Console.WriteLine("Вторник");
+}
+else if (num == "3")
+{
+Console.WriteLine("Среда");
+}
+else if (num == "4")
+{
+Console.WriteLine("Четверг");
+}
+else if (num == "5")
+{
+Console.WriteLine("Пятница");
+}
+else if (num == "6")
+{
+Console.WriteLine("Суббота");
+}
+else if (num == "7")
+{
+Console.WriteLine("Воскресенье");
 }
 else
 {
-Console.WriteLine("Введено неправильное число.");
+Console.WriteLine("Некорректный воод");
 }
