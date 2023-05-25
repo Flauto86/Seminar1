f10e37d7dc7bbb462a4a6f5b954a3f827eb045eb
Fedor Romanov <flauto86@gmail.com>
2023-05-25 09:35:04 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+    Console.WriteLine("Некорректный ввод");
+else{
+    int i = 2;
+    while (i <= number)
+    {
+        Console.Write($"{i} ");
+        i = i + 2;
+    }
+}
